66190c7b438d5c859ec653089271c274f3b093bf
Shahid Hasan <shahidhasan0607@gmail.com>
2020-01-29 14:27:33 +0530

    helo message changed and resource folder added

--- src/com/example/HelloClient.java
+++ src/com/example/HelloClient.java
@@ -4,7 +4,7 @@ public class HelloClient {
 
 	public static void main(String[] args) {
 	 
-			System.out.println("Hello from Hasan !!");
+			System.out.println("Hello from Shahid Hasan !!");
 
 	}
 
